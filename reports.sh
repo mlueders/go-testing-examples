55a9e3cbed94c6cc314bb4fdd8dc1f59b8ae1b0d
Michael Lueders <mike@roadkill.org>
2022-04-02 18:51:01 -0500

    add pass output to reports

--- reports.sh
+++ reports.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-mkdir reports
-rm reports/*
+rm -rf reports/
+mkdir -p reports/pass
+mkdir -p reports/fail
 
-export SHOULD_FAIL=true
 for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
     branchName=`echo "${branch##*/}"`
     git checkout $branch
-    go test -v > "reports/${branchName}.out"
+    export SHOULD_FAIL=false
+    go test -v > "reports/pass/${branchName}.out"
+    export SHOULD_FAIL=true
+    go test -v > "reports/fail/${branchName}.out"
 done
 
 git checkout main
